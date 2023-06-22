18eb4a3f5f906ea5e613ad7b6402594edf7b2fc7
Jack <jackwebdev31@gmail.com>
2023-06-22 15:47:54 +0200

    working on header

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  content: ['./src/**/*.html', './src/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        'main-bg': '#F3F2F2',
+      }
+    },
+  },
+  plugins: [],
+}
+
